fad29606d3ed970676fc553b028b93e9e94ea9d7
beck.bi <cabing_2005@126.com>
2014-07-24 14:12:20 +0800

    add mysqli class

--- php_ext/DB/Db.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace DB;
-interface database{
-    public function connect();
-    public function fetchOne($sql);
-    public function fetchRows($sql);
-    public function query($sql);
-}
