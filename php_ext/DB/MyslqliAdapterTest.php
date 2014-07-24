6177fa1992c6277f0e498163795d898e81b3dfea
beck.bi <cabing_2005@126.com>
2014-07-24 14:09:04 +0800

    add a phpunit class which is not completed

---
+++ php_ext/DB/MyslqliAdapterTest.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'MyslqliAdapter.php';
+
+class MyslqliAdapterTest extends PHPUnit_Framework_TestCase {
+
+    public function test_getKeys() {
+
+    }
+    public function test_getEncryptKeys() {
+
+    }
+    public function test_connect() {
+
+    }
+
+    public function test_fetchOne() {
+
+    }
+
+    public function test_fetchRows() {
+
+    }
+
+}
